4af61f7b2cd60513bc77009d34ff98e101d35238
Sindhura492 <sindhura7649@gmail.com>
2024-06-09 16:08:52 +0200

    user role update and address update done

--- backend/chemnitz_project/category_api/categories.py
+++ backend/chemnitz_project/category_api/categories.py
@@ -7,32 +7,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-
-
-# class KindergartenView(APIView):
-#     def get(self, request, *args, **kwargs):
-#         kindergartens = Kindergarten.objects.all()
-#         serializer = KindergartenSerializer(kindergartens, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
-# class SchulennView(APIView):
-#     def get(self, request, *args, **kwargs):
-#         schules = Schulen.objects.all()
-#         serializer = SchulenSerializer(schules, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-# class JugendberufshilfenView(APIView):
-#     def get(self, request, *args, **kwargs):
-#         jugendberufshilfens = Jugendberufshilfen.objects.all()
-#         serializer = JugendberufshilfenSerializer(jugendberufshilfens, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-    
-# class SchulsozialarbeitView(APIView):
-#     def get(self, request, *args, **kwargs):
-#         schulsozialarbeits = Schulsozialarbeit.objects.all()
-#         serializer = SchulsozialarbeitSerializer(schulsozialarbeits, many=True)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-
 class KindergartenView(APIView):
  def get(self, request, *args, **kwargs):
         user_id = request.user.id
--- backend/chemnitz_project/user_api/users.py
+++ backend/chemnitz_project/user_api/users.py
@@ -42,8 +42,28 @@ class DeleteUserView(generics.UpdateAPIView):
         auth_user.is_active = False
         auth_user.save()
         serializer = self.get_serializer(user)
-        return Response({"message": "User deleted successfully."}, status=status.HTTP_200_OK)
+        return Response({"detail": "User deleted successfully."}, status=status.HTTP_200_OK)
     
 class InactiveUserListView(generics.ListAPIView):
     queryset = Users.objects.filter(is_active=False)
     serializer_class = UserViewSerializer
+
+class ChangeUserRole(APIView):
+    def put(self, request, user_id):
+        user_profile = get_object_or_404(Users, user_id=user_id)
+
+        if user_profile.role == 2:
+            user_profile.role = 1   
+            message = "User role has been upgraded from Regular to Super!."
+        elif user_profile.role == 1:
+            try:
+                UserAddress.objects.filter(user=user_profile).delete()
+                Favorite.objects.filter(user=user_profile).delete()
+            except:
+                pass
+            user_profile.role = 2
+            message = "User role has been changed from Super to Regular!."
+
+        user_profile.save()
+        serializer = UserViewSerializer(user_profile)
+        return Response({"detail": message, "data": serializer.data}, status=status.HTTP_200_OK)
